72ecb5bfdbab4ccebfe23e9f44e001e95b5d1c7a
Santiago Velosa <santiagovelosa@gmail.com>
2022-03-21 19:36:35 -0500

    Edit hello.html.erb

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  resources :pages
+
+  root "pages#hello"
+
+  get "pages/index", to:"pages#index"
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
